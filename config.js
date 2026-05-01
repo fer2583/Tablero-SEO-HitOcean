7042c834b173d86de3bf6fd12a52911027cfd66a
fer2583 <131730168+fer2583@users.noreply.github.com>
2026-05-01 19:14:28 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SHEET_ID: "1RKWE0S9HZPCiMy_ds-5KJEaGqPVslv8C",
   SHEET_URL: "https://docs.google.com/spreadsheets/d/1RKWE0S9HZPCiMy_ds-5KJEaGqPVslv8C/edit?usp=sharing&ouid=103185604411661124832&rtpof=true&sd=true",
-  DATA_URL: "PEGAR_AQUI_URL_DEL_APPS_SCRIPT_EXEC"
+  DATA_URL: "https://script.google.com/macros/s/AKfycbzUsfF3qMrqvI2rvGZdQfQZc1GaTMYkhWcpgvnkZ1Of7_BsAKEYj_hsKOtrGQLru0tpNA/exec"
 };
